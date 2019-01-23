afcda196017b0e53309d19ced8c285ce17dc217b
Andrew Koslow <andrewkoslow@gmail.com>
2019-01-23 13:49:00 +0300

    Clean project created

---
+++ Recurrency/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 23/01/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
