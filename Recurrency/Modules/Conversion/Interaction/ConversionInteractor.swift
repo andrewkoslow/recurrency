63f90e60b5ba9a68d5ad6978e47b23761c103b76
Andrew Koslow <andrewkoslow@gmail.com>
2019-01-29 02:15:23 +0300

    ConversionInteractor added

---
+++ Recurrency/Modules/Conversion/Interaction/ConversionInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionInteractor.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 29/01/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+class ConversionInteractor {
+    
+}
