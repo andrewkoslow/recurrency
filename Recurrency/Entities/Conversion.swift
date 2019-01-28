2de175d6627d561c6091baf312bbd69b112320c5
Andrew Koslow <andrewkoslow@gmail.com>
2019-01-29 02:06:45 +0300

    Conversion entity added

---
+++ Recurrency/Entities/Conversion.swift
@@ -0,0 +1,41 @@
+//
+//  Conversion.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 29/01/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+struct Conversion {
+    
+    enum Error: Swift.Error {
+        case ratesContainsBase
+        case rateLessThanOrEqualsZero
+    }
+    
+    private let rates: [Currency: Decimal]
+    
+    var currencies: [Currency] {
+        return Array(rates.keys)
+    }
+    
+    init(base: Currency, rates: [Currency: Decimal]) throws {
+        guard rates.keys.contains(base) == false else { throw Error.ratesContainsBase }
+        guard rates.values.contains(where: { $0 <= 0 }) == false else { throw Error.rateLessThanOrEqualsZero }
+        
+        var rates = rates
+        rates[base] = 1
+        
+        self.rates = rates
+    }
+    
+    func convert(amount: Decimal, from source: Currency, to target: Currency) -> Decimal? {
+        guard let denominator = rates[source] else { return nil }
+        guard let multiplicator = rates[target] else { return nil }
+        
+        return multiplicator / denominator * amount
+    }
+    
+}
