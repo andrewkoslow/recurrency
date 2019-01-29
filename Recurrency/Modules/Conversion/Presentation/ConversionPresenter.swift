3a2a1653591bb422f99fc52b1e089ff4af84ab57
Andrew Koslow <andrewkoslow@gmail.com>
2019-01-29 14:32:07 +0300

    ConversionPresenter added

---
+++ Recurrency/Modules/Conversion/Presentation/ConversionPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  ConversionPresenter.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 29/01/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+class ConversionPresenter {
+    
+    private let viewController: ConversionViewControllerProtocol
+    
+    init(viewController: ConversionViewControllerProtocol) {
+        self.viewController = viewController
+    }
+    
+}
