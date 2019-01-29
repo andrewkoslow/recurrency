f2e7fb79330763efd1e96095bc942ba96f88bad0
Andrew Koslow <andrewkoslow@gmail.com>
2019-01-29 14:30:44 +0300

    ConversionViewControllerProtocol added

---
+++ Recurrency/Modules/Conversion/Presentation/ConversionViewControllerProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  ConversionViewControllerProtocol.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 29/01/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+struct ConversionViewModel {
+    
+    struct Amount {
+        let currency: Currency
+        let amount: Decimal?
+    }
+    
+    let amounts: [Amount]
+    
+}
+
+protocol ConversionViewControllerProtocol: AnyObject {
+    
+    func update(with model: ConversionViewModel)
+    
+}
