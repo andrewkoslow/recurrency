64e83e468e7c22d040e553f0d1429f5e76a23010
Andrew Koslow <andrewkoslow@gmail.com>
2019-02-02 17:02:06 +0300

    Amount entity added

---
+++ Recurrency/Entities/Amount.swift
@@ -0,0 +1,16 @@
+//
+//  Amount.swift
+//  Recurrency
+//
+//  Created by Andrew Koslow on 02/02/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+struct Amount {
+    
+    var currency: Currency
+    var value: Decimal?
+    
+}
