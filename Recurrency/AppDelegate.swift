a6599f7827999b374f9819008ec030abec48c3ad
Andrew Koslow <andrewkoslow@gmail.com>
2019-02-03 15:36:39 +0300

    AppDelegate extended with starting conversion interaction

--- Recurrency/AppDelegate.swift
+++ Recurrency/AppDelegate.swift
@@ -11,7 +11,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    var conversionInteractor: ConversionInteractionProtocol?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let conversionInteractor = ConversionInteractorBuilder(window: window).build()
+        conversionInteractor.start()
+        
+        window.makeKeyAndVisible()
+        
+        self.conversionInteractor = conversionInteractor
+        self.window = window
+        
         return true
     }
     
