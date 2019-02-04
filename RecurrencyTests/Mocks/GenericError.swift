acb9882277bbd51b32d977129fd53873398b1a87
Andrew Koslow <andrewkoslow@gmail.com>
2019-02-04 12:13:40 +0300

    Test GenericError added

---
+++ RecurrencyTests/Mocks/GenericError.swift
@@ -0,0 +1,11 @@
+//
+//  GenericError.swift
+//  RecurrencyTests
+//
+//  Created by Andrew Koslow on 04/02/2019.
+//  Copyright © 2019 Andrew Koslow. All rights reserved.
+//
+
+import Foundation
+
+struct GenericError: Error {}
